08c0d6283d28d5ef22e7ac4b524c49ebb679163c
soarflat <soarflat777@gmail.com>
2017-08-08 00:21:49 +0900

    update static-files

--- static-files/app.js
+++ static-files/app.js
@@ -12,4 +12,10 @@ app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
 
+/**
+ * app.useは指定されたミドルウェア関数を指定したパスで実行する
+ * 今回はexpress.staticミドルウェア関数をpublicパスで実行する
+ * express.staticはExpressの唯一の標準組み込みのミドルウェア関数であり
+ * 静的アセットファイルを提供する
+ */
 app.use(express.static('public'));
