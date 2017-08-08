e10fbf324b58a5c3adcaa5e55461ba9a2bdad2de
soarflat <soarflat777@gmail.com>
2017-08-08 21:23:40 +0900

    add body-parser example

--- body-parser/app.js
+++ body-parser/app.js
@@ -1,3 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+// application/x-www-form-urlencodedをパースする
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/jsonをパースする
+app.use(bodyParser.json());
+
 /**
- * Created by mac on 2017/08/08.
+ * curl -v -H 'Content-type:application/json' -X POST -d '{"firstName":"Taro","lastName":"Suzuki"}'  http://localhost:3000/form
  */
+app.post('/form', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({
+    firstName: req.body.firstName || null,
+    lastName: req.body.lastName || null
+  }));
+
+  console.log('you posted: First Name: ' + req.body.firstName + ', Last Name: ' + req.body.lastName);
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
